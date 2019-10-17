9c179a8abbfb8228ffd78ba93e30d1f1a0a16b9b
Hutch Crowley <tothecoda@gmail.com>
2019-10-17 14:02:36 -0500

    Update fix all problems. Need stretch goals.

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,28 +1,25 @@
-/* Step 1: using axios, send a GET request to the following URL
-           (replacing the palceholder with your Github name):
-           https://api.github.com/users/<your name>
-*/
 
-
-
-
-
+// Axios HTTP 'get' request
 
 axios.get('https://api.github.com/users/hutchcrowley')
   .then(response =>
   {
     console.log(response);
 
-    const githubUser = response.data;
+    const gitHubUser = response.data;
 
     const cardParent = document.querySelector('.cards');
-    cardParent.appendChild(githubCardComponent(githubUser))
+    cardParent.appendChild(githubCardComponent(gitHubUser));
 
   })
   .catch(err =>
   {
+
     console.log(err);
+
   });
+
+// Array that feeds constuctor function
 
 const followersArray = [
 
@@ -34,12 +31,13 @@ const followersArray = [
   'bigknell',
 ];
 
+//  First constuctor function. takes an array as its one parameter and iterates over every index of the 'followersArray' array. For each iteration of the function, it fires off the callback which creates a new card based on the second constructor function, githubCardComponent.
+
+
 followersArray.forEach(user =>
 {
 
-
-
-  axios.get(`https://api.github.com/users/${user}`)
+  axios.get('https://api.github.com/users/' + user)
     .then(response =>
     {
       console.log(response.data);
@@ -50,24 +48,26 @@ followersArray.forEach(user =>
       cardParent.appendChild(githubCardComponent(userObj));
 
     })
+
     .catch(err =>
     {
       console.log(err);
-    });
-
+    })
 });
 
+// This is a constructor function that contains the model for the data passed into it by the previous function. First, new elements are created using document.createElement. Second, attributes, class names, etc are set using various methods. The goal being to get the elements to line up with the layout of the HTML, as well as the existing CSS. Third, the elements, processed through the constructor function, areadded to the dom and Presto! A new card is dynamically created.
 
 
-
-function githubCardComponent (githubUser)
+const githubCardComponent = (gitHubUser =>
 {
+
+  // First
 
   const cardContainer = document.createElement("div");
   const profileImage = document.createElement("img");
   const cardInformationContainer = document.createElement("div");
   const userName = document.createElement("h3");
-  const usernameName = document.createElement("p");
+  const userUserName = document.createElement("p");
   const location = document.createElement("p");
   const profile = document.createElement("p");
   const urlAddress = document.createElement("a");
@@ -75,70 +75,42 @@ function githubCardComponent (githubUser)
   const following = document.createElement("p");
   const bio = document.createElement("p");
 
+  // Second
+  console.log(gitHubUser);
+
+  cardContainer.classList.add("card");
+  profileImage.src = gitHubUser.avatar_url;
+  cardInformationContainer.classList.add("card-info");
+  userName.classList.add("users-name");
+  userName.textContent = gitHubUser.name;
+  userUserName.classList.add("users-user-name");
+  userUserName.textContent = gitHubUser.login;
+  urlAddress.setAttribute("href", gitHubUser.html_url);
+  urlAddress.textContent = gitHubUser.html_url;
+  location.textContent = 'location' + gitHubUser.location;
+  followers.textContent = 'Followers: ' + gitHubUser.followers;
+  following.textContent = 'Following: ' + gitHubUser.following;
+  bio.textContent = 'Bio: ' + gitHubUser.bio;
+
+  // Third
+  console.log(gitHubUser);
 
   cardContainer.appendChild(profileImage);
   cardContainer.appendChild(cardInformationContainer);
+
   cardInformationContainer.appendChild(userName);
-  cardInformationContainer.appendChild(usernameName);
+  cardInformationContainer.appendChild(userUserName);
   cardInformationContainer.appendChild(location);
   cardInformationContainer.appendChild(profile);
+
   profile.appendChild(urlAddress);
+  console.log(gitHubUser);
+
   cardInformationContainer.appendChild(followers);
   cardInformationContainer.appendChild(following);
   cardInformationContainer.appendChild(bio);
 
-
-  cardContainer.classList.add("card");
-  cardInformationContainer.classList.add("card-info");
-  userName.classList.add("name");
-  usernameName.classList.add("username");
-  profileImage.src = githubUser.avatar_url;
-  userName.textContent = githubUser.name;
-  usernameName.textContent = githubUser.login;
-  urlAddress.textContent = githubUser.html_url;
-  location.loctextContent = `Location: ${githubUser.location}`;
-  followers.textContent = `Followers: ${githubUser.followers}`;
-  following.textContent = `Followers: ${githubUser.followers}`;
-  profile.textContent = ("Profile: ");
-
-  urlAddress.setAttribute("href", githubUser.html_url);
-
-  bio.textContent = `Bio: ${githubUser.bio}`;
   console.log(cardContainer);
+
   return cardContainer;
-}
-
-/* Step 5: Now that you have your own card getting added to the DOM, either
-          follow this link in your browser https://api.github.com/users/<Your github name>/followers
-          , manually find some other users' github handles, or use the list found
-          at the bottom of the page. Get at least 5 different Github usernames and add them as
-          Individual strings to the friendsArray below.
-
-          Using that array, iterate over it, requesting data for each user, creating a new card for each
-          user, and adding that card to the DOM.
-*/
-
-
-
-
-// Step 3: Create a function that accepts a single object as its only argument,
-//           Using DOM methods and properties, create a component that will return the following DOM element:
-
-// <div class="card">
-//   <img src={image url of user} />
-//   <div class="card-info">
-//     <h3 class="name">{users name}</h3>
-//     <p class="username">{users user name}</p>
-//     <p>Location: {users location}</p>
-//     <p>Profile:
-//       <a href={address to users github page}>{address to users github page}</a>
-//     </p>
-//     <p>Followers: {users followers count}</p>
-//     <p>Following: {users following count}</p>
-//     <p>Bio: {users bio}</p>
-//   </div>
-// </div>
-//
-//
-// /* List of LS Instructors Github username's:
-//
+});
